1962a30956fc312865cdb060f428d611862c2806
Ergo322 <Shikmen2@mail.ru>
2022-09-22 19:51:38 +0300

    task68

---
+++ Задача68/Program.cs
@@ -0,0 +1,11 @@
+﻿int Akkerman(int M, int N)
+{
+  if (M == 0)
+    return N + 1;
+  else
+    if ((M != 0) && (N == 0))
+      return Akkerman(M - 1, 1);
+    else
+      return Akkerman(M - 1, Akkerman(M, N - 1));
+}
+Console.WriteLine($"{Akkerman(3,2)}");
